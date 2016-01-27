a618a4ecbedfba533ae44834cb1f63923ca430c3
HenryXi <xxy668@foxmail.com>
2016-01-27 16:49:42 +0800

    init commit

---
+++ interface/src/main/java/com/henry/xi/hessian/MathService.java
@@ -0,0 +1,10 @@
+package com.henry.xi.hessian;
+
+/**
+ * Created by admin on 2016/1/27.
+ */
+public interface MathService {
+
+    public int add(int a, int b);
+
+}
---
+++ server/src/main/java/com/henry/xi/hessian/impl/HessianMathService.java
@@ -0,0 +1,14 @@
+package com.henry.xi.hessian.impl;
+
+import com.henry.xi.hessian.MathService;
+
+/**
+ * Created by admin on 2016/1/27.
+ */
+public class HessianMathService implements MathService {
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+}
